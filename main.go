169cc36eee9e2ff3ae6aea6783916111f7543d02
Alexsandro Specht <alexsandrospecht@gmail.com>
2017-10-10 21:21:54 -0300

    ADD Mongo database

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"html/template"
 	"net/http"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"fmt"
+	"log"
 )
 
 type Dojo struct {
+	Id    int
 	Title string
 	Done  bool
 }
@@ -18,14 +24,45 @@ type DojoPageData struct {
 func main() {
 	tmpl := template.Must(template.ParseFiles("pages/layout.html"))
 
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	c := session.DB("dojo").C("dojo")
+
+
+	count, err2 := c.Find(bson.M{}).Count()
+	if err2 != nil {
+		panic(err)
+	}
+
+	// FOR TEST
+	if count == 0 {
+		dojoteste1 := Dojo{Id: 1, Title: "Teste 1", Done: false}
+		c.Insert(dojoteste1)
+
+		dojoteste2 := Dojo{Id: 2, Title: "Teste 2", Done: false}
+		c.Insert(dojoteste2)
+
+		dojoteste3 := Dojo{Id: 3, Title: "Teste 3", Done: false}
+		c.Insert(dojoteste3)
+
+		dojoteste4 := Dojo{Id: 4, Title: "Teste 4", Done: false}
+		c.Insert(dojoteste4)
+	}
+
+	var dojos [] Dojo
+	c.Find(bson.M{}).All(&dojos)
+
+	fmt.Println(dojos[0].Title)
+
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := DojoPageData{
 			PageTitle: "Dojo's",
-			Dojos: []Dojo{
-				{Title: "Dojo 1", Done: false},
-				{Title: "Dojo 2", Done: true},
-				{Title: "Dojo 3", Done: true},
-			},
+			Dojos: dojos,
 		}
 		tmpl.Execute(w, data)
 	})
