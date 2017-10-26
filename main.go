bee7ae93cf4a544d86ab4a69475d0753ff6f5c57
Alexsandro Specht <alexsandrospecht@gmail.com>
2017-10-25 22:51:29 -0200

    DojoHandler and config yaml

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"html/template"
 	"net/http"
-
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"fmt"
 	"log"
+	"github.com/creamdog/gonfig"
+	"os"
 )
+
+type Config struct {
+	Database string
+}
+
+type DojoHandler struct {
+	db *mgo.Collection
+}
 
 type Dojo struct {
 	Id    int
@@ -22,21 +31,32 @@ type DojoPageData struct {
 }
 
 func main() {
-	http.HandleFunc("/", loadAll)
-	http.HandleFunc("/dojo", dojo)
+	config := loadConfig()
+
+	session := config.getSession()
+	defer session.Close()
+
+	h := DojoHandler{db: session.DB("dojo").C("dojo")}
+
+	http.HandleFunc("/", h.loadAll)
+	http.HandleFunc("/dojo", h.dojo)
 
 	http.ListenAndServe(":3000", nil)
 }
 
-func loadAll(w http.ResponseWriter, r *http.Request) {
+func loadConfig() Config {
+	f, _ := os.Open("properties.yml")
+	defer f.Close()
+	config, _ := gonfig.FromYml(f)
+	database, _ := config.GetString("database", "")
+	return Config{Database: database}
+}
+
+func (h *DojoHandler) loadAll(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("pages/layout.html"))
 
-	session := getSession()
-	defer session.Close()
-
-	c := session.DB("dojo").C("dojo")
 	var dojos [] Dojo
-	c.Find(bson.M{}).All(&dojos)
+	h.db.Find(bson.M{}).All(&dojos)
 
 	data := DojoPageData{
 		PageTitle: "Dojo's",
@@ -46,28 +66,22 @@ func loadAll(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func dojo(w http.ResponseWriter, r *http.Request) {
+func (h *DojoHandler) dojo(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
 		t := template.Must(template.ParseFiles("pages/dojo.html"))
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
-		// logic part of log in
 		fmt.Println("title:", r.Form["title"])
 
-		session := getSession()
-		defer session.Close()
-
-		c := session.DB("dojo").C("dojo")
-		c.Insert(Dojo{Id: 0, Title: r.Form["title"][0], Done: false})
-
+		h.db.Insert(Dojo{Id: 0, Title: r.Form["title"][0], Done: false})
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
 
-func getSession() *mgo.Session {
-	session, err := mgo.Dial("localhost")
+func (config *Config) getSession() *mgo.Session {
+	session, err := mgo.Dial(config.Database)
 
 	if err != nil {
 		log.Fatal(err)
